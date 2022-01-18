ae68be3ddb8647b0b4ffb949deb9ed3ca3eed03c
gunner10z <24jzweiback@athenian.org>
2022-01-18 10:11:11 -0800

    asdf

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class MyMain {
     // This is the example problem that we did in class
     // This uses recursion to calculate the factorial method
@@ -23,8 +24,13 @@ public class MyMain {
     // arithmeticSequence(4) => 9
     // arithmeticSequence(1) => 3
     public static int arithmeticSequence(int n) {
-        // YOUR CODE HERE
-        return -1;
+
+        if (n==1){
+            return 3;
+        }
+        else{
+            return arithmeticSequence(n-1)+2;
+        }
     }
 
     // Write a method that uses recursion to count the number
@@ -36,8 +42,15 @@ public class MyMain {
     // Hint: to make the subproblem smaller, make the String shorter!
     // Hint 2: multiple recursive cases might be helpful!
     public static int countX(String str) {
-        // YOUR CODE HERE
-        return -1;
+        if(str.length()==0) {
+            return 0;
+        }
+
+        if ((str.substring(0,1).equals("x"))) {
+            return 1 + countX(str.substring(1));
+        }
+
+        return countX(str.substring(1));
     }
 
 
@@ -52,8 +65,14 @@ public class MyMain {
     // exponent(5, 2) = 5 * 5 = 25
     // exponent(4, 0) = 1
     public static int exponent(int base, int power) {
-        // YOUR CODE HERE
-        return -1;
+        if (power<1){
+            return 1;
+        }
+        else{
+
+            return base*exponent(base, power-1);
+        }
+        //return -1;
     }
 
     // Write a method that uses recursion to count the number
@@ -62,13 +81,23 @@ public class MyMain {
     // countHi("hix") => 1
     // countHi("hellohihi") => 2
     public static int countHi(String str) {
-        // YOUR CODE HERE
-        return -1;
+        if(str.length()==1) {
+            return 0;
+        }
+
+        if ((str.substring(0,2).equals("hi"))) {
+            return 1 + countHi(str.substring(1));
+        }
+
+        return countHi(str.substring(1));
     }
+
 
 
     public static void main(String[] args) {
         // Optional: Write some code here to test your methods!
+        //countX("hix");
+        countHi("hix");
         System.out.println(factorial(5));
     }
 }
